090ebd219d1ea0739ef3997dc46ec58a9d314440
Tomas <tomizechsterson@gmail.com>
2022-07-31 20:46:24 -0500

    Update a few top-level things
    Post layout, index, site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,11 +3,12 @@ const siteMetadata = {
   author: 'Tomas Eychaner',
   headerTitle: 'My Site That Has Things',
   description:
-    'This site will eventually have things. With any luck, these things will be meaningful and/or valuable in some way.',
+    'This site will eventually have things. With any luck, these things will be meaningful and/or valuable in some way. ' +
+    "Ideally, they'll be valuable for someone other than me, but let's not get ahead of ourselves.",
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: '',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
