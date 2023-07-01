0a89a592f854a28f4a01382cc10bb8b5c64d70a2
Tomas <tomizechsterson@users.noreply.github.com>
2023-07-01 17:50:33 -0500

    Fix project images and add one for Asteroids
    They will now stay in the container instead of overflowing past the card border.

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,7 +2,7 @@ const projectsData = [
   {
     title: 'Asteroids Tutorial',
     description: `A Godot tutorial that gives you a good start to an Asteroids-like arcade shooter.`,
-    imgSrc: '/static/images/godot_logo_large.png',
+    imgSrc: '/static/images/godot_logo_asteroids.png',
     href: '/blog/godot-asteroids-project'
   }
 ]
