a8aa094e1b52f24ce15e5d1aaa9a7272412fad7a
Tomas <tomizechsterson@users.noreply.github.com>
2023-07-01 19:52:36 -0500

    Small change to test comment removal

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,7 +1,7 @@
 const projectsData = [
   {
     title: 'Asteroids Tutorial',
-    description: `A Godot project walkthrough that gives you a good start to an Asteroids-like arcade shooter.`,
+    description: `A Godot project walkthrough that gives you a good start to an Asteroids-like arcade shooter`,
     imgSrc: '/static/images/godot_logo_asteroids.png',
     href: '/blog/godot-asteroids-project-initial'
   }
