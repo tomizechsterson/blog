97645323d6d80e22c30efc0c616c9f682bc6122c
Tomas <tomizechsterson@users.noreply.github.com>
2023-07-09 13:42:47 -0500

    Run linter manually

--- layouts/ProjectLayout.js
+++ layouts/ProjectLayout.js
@@ -9,7 +9,7 @@ export default function ProjectLayout({ children, frontMatter, projects }) {
     <>
       <PageSEO title={`Projects - ${name}`} description={`Projects - ${name}`} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {name}
           </h1>
@@ -58,7 +58,7 @@ export default function ProjectLayout({ children, frontMatter, projects }) {
               })}
             </ul>
           </div>
-          <div className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">{children}</div>
+          <div className="prose max-w-none pb-8 pt-8 dark:prose-dark xl:col-span-2">{children}</div>
         </div>
       </div>
     </>
