db972020388008721578a0a4e80c71a9a4fe5bde
Tomas <tomizechsterson@users.noreply.github.com>
2023-07-09 21:13:04 -0500

    Add step for uploading screenshots on failure
    Also ensure that videos don't get generated when tests pass...

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     video: false,
+    videoUploadOnPasses: false,
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
