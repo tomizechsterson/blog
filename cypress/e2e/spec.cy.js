e1320a76e3684e751931712c3424a1898be47803
Tomas <tomizechsterson@users.noreply.github.com>
2023-07-09 19:49:29 -0500

    Add Cypress for integration tests

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+describe('Home Page', () => {
+  it('should navigate to the home page', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('h1').contains('Latest Things')
+    cy.get('a').contains('Blog')
+    cy.get('a').contains('Tags')
+    cy.get('a').contains('Projects')
+    cy.get('a').contains('About')
+  })
+})
