a174cbbd21ae640246261ceb613be785038a75a9
Tomas <tomizechsterson@users.noreply.github.com>
2023-07-11 00:25:57 -0500

    Redo tags and about tests

--- cypress/e2e/about.cy.js
+++ cypress/e2e/about.cy.js
@@ -2,9 +2,8 @@ describe('About Page', () => {
   it('should display the about page and some details', () => {
     cy.visit('/about')
 
-    cy.get('h1').contains('About')
-    cy.get('h3').contains('Tomas Eychaner')
-    cy.get('div').contains('Software Engineer')
+    cy.get('h1').should('have.text', 'About')
+    cy.get('h3').should('have.text', 'Tomas Eychaner')
     cy.get('a[href*="mailto:"]').should('have.length', 2)
     cy.get('a[href="https://github.com/tomizechsterson"]').should('have.length', 2)
     cy.get('a[href="https://www.linkedin.com"]').should('have.length', 2)
--- cypress/e2e/tags.cy.js
+++ cypress/e2e/tags.cy.js
@@ -1,34 +1,23 @@
 describe('Tags', () => {
-  it('navigates to the first tag', () => {
+  it('navigates to the first tag and the first post within', () => {
     cy.visit('/tags')
 
-    cy.get('h1').contains('Tags')
-    cy.get('a[href^="/tags/"]')
-      .first()
-      .then((a) => {
-        cy.get(a).invoke('attr', 'href').as('href')
-        cy.get(a).click()
-      })
+    cy.get('h1').should('have.text', 'Tags')
+    cy.get('a[href^="/tags/"]').first().as('tagLink').click()
 
-    cy.get('@href').then((href) => {
-      cy.location('pathname').should('eq', href)
+    cy.get('@tagLink').then((link) => {
+      cy.location('pathname').should('eq', link.attr('href'))
     })
 
     cy.get('article')
       .first()
       .within(() => {
-        cy.get('a[href^="/blog"]').then(($a) => {
-          cy.get($a).invoke('text').as('anchorText')
-          cy.get($a).invoke('attr', 'href').as('href')
-          cy.get($a).click()
-        })
+        cy.get('a[href^="/blog"]').as('articleLink').click()
       })
 
-    cy.get('@anchorText').then((text) => {
-      cy.get('h1').contains(text)
-    })
-    cy.get('@href').then((href) => {
-      cy.location('pathname').should('eq', href)
+    cy.get('@articleLink').then((link) => {
+      cy.location('pathname').should('eq', link.attr('href'))
+      cy.get('h1').should('have.text', link.text())
     })
   })
 })
