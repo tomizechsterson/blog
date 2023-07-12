7d4fee1143abffa974c1f6e30c5ea62b4ebdf7d3
Tomas <tomizechsterson@users.noreply.github.com>
2023-07-11 20:19:11 -0500

    Add navigation to first item on home page
    Also use better assertions and try using siteMetadata

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,19 +1,46 @@
+import siteMetadata from '../../data/siteMetadata'
+
 describe('Home Page', () => {
   it('should have header, top nav, and footer', () => {
     cy.visit('/')
 
-    cy.get('h1').contains('Latest Things')
+    cy.get('title').should('have.text', siteMetadata.title)
+    cy.get('h1').should('have.text', 'Latest Things')
 
-    cy.get('a').contains('Blog')
-    cy.get('a').contains('Tags')
-    cy.get('a').contains('Projects')
-    cy.get('a').contains('About')
+    cy.get('header').within(() => {
+      cy.get('a').should('contain.text', 'Blog')
+      cy.get('a').should('contain.text', 'Tags')
+      cy.get('a').should('contain.text', 'Projects')
+      cy.get('a').should('contain.text', 'About')
+    })
 
-    cy.get('a[href*="mailto:"]').should('have.length', 1)
-    cy.get('a[href="https://github.com/tomizechsterson"]').should('have.length', 1)
-    cy.get('a[href="https://www.linkedin.com"]').should('have.length', 1)
-    cy.get('a[href="https://app.pluralsight.com/profile/tomaseychaner"]').should('have.length', 1)
-    cy.get('a[href="https://rumble.com/user/tomizechsterson"]').should('have.length', 1)
-    cy.get('a').contains("Tomas's Site").should('have.length', 1)
+    cy.get('footer').within(() => {
+      cy.get('a[href*="mailto:"]').should('have.length', 1)
+      cy.get('a[href="https://github.com/tomizechsterson"]').should('have.length', 1)
+      cy.get('a[href="https://www.linkedin.com"]').should('have.length', 1)
+      cy.get('a[href="https://app.pluralsight.com/profile/tomaseychaner"]').should('have.length', 1)
+      cy.get('a[href="https://rumble.com/user/tomizechsterson"]').should('have.length', 1)
+      cy.get('a').contains("Tomas's Site").should('have.length', 1)
+    })
+  })
+
+  it('should navigate to first item and back via header', () => {
+    cy.visit('/')
+
+    cy.get('article').should('have.length', 5)
+    cy.get('article')
+      .first()
+      .within(() => {
+        cy.get('a').first().as('link').click()
+      })
+
+    cy.get('@link').then((link) => {
+      cy.location('pathname').should('eq', link.attr('href'))
+      cy.get('h1').should('have.text', link.text())
+    })
+
+    cy.get('a[aria-label="A Site That Has Things"]').click()
+
+    cy.location('pathname').should('eq', '/')
   })
 })
