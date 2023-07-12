06a48d0ef6c7c570e8a349fcce95e307100607b0
Tomas <tomizechsterson@users.noreply.github.com>
2023-07-11 19:24:53 -0500

    Redo blog tests
    Thankfully, they all work now

--- cypress/e2e/blog.cy.js
+++ cypress/e2e/blog.cy.js
@@ -2,27 +2,23 @@ describe('Blog', () => {
   it('should display list of posts and navigate around them', () => {
     cy.visit('/blog')
 
-    cy.get('h1').contains('All Posts')
+    cy.get('h1').should('have.text', 'All Posts')
     cy.get('article').should('have.length', 5)
-    cy.get('[aria-label="Search articles"]').type('retro platformer')
+    cy.get('[aria-label="Search articles"]')
+      .type('retro platformer')
+      .should('have.value', 'retro platformer')
     cy.get('article').should('have.length', 1)
 
     cy.get('article').within(() => {
-      cy.get('a[href^="/blog"]').then(($a) => {
-        cy.get($a).invoke('text').as('anchorText')
-        cy.get($a).invoke('attr', 'href').as('href')
-        cy.get($a).click()
-      })
+      cy.get('a[href^="/blog"]').as('articleLink').click()
     })
 
-    cy.get('@anchorText').then((text) => {
-      cy.get('h1').contains(text)
+    cy.get('@articleLink').then((link) => {
+      cy.location('pathname').should('eq', link.attr('href'))
+      cy.get('h1').should('have.text', link.text())
     })
-    cy.get('@href').then((href) => {
-      cy.location('pathname').should('eq', href)
-    })
-    cy.get('div').contains('min read')
-    cy.get('h2').contains('Tags')
+    cy.get('div').should('contain.text', 'min read')
+    cy.get('h2').should('contain.text', 'Tags')
     cy.get('h2')
       .contains('Tags')
       .parent()
@@ -34,41 +30,27 @@ describe('Blog', () => {
       .contains('Previous Article')
       .parent()
       .within(() => {
-        cy.get('a[href^="/blog"]').then(($a) => {
-          cy.get($a).invoke('text').as('anchorText')
-          cy.get($a).invoke('attr', 'href').as('href')
-          cy.get($a).click()
-        })
+        cy.get('a[href^="/blog"]').as('articleLink').click()
       })
 
-    cy.get('@anchorText').then((text) => {
-      cy.get('h1').contains(text)
-    })
-    cy.get('@href').then((href) => {
-      cy.location('pathname').should('eq', href)
+    cy.get('@articleLink').then((link) => {
+      cy.location('pathname').should('eq', link.attr('href'))
+      cy.get('h1').should('have.text', link.text())
     })
 
     cy.get('h2')
       .contains('Next Article')
       .parent()
       .within(() => {
-        cy.get('a[href^="/blog"]').then(($a) => {
-          cy.get($a).invoke('text').as('anchorText')
-          cy.get($a).invoke('attr', 'href').as('href')
-          cy.get($a).click()
-        })
+        cy.get('a[href^="/blog"]').as('articleLink').click()
       })
 
-    cy.get('@anchorText').then((text) => {
-      cy.get('h1').contains(text)
+    cy.get('@articleLink').then((link) => {
+      cy.location('pathname').should('eq', link.attr('href'))
+      cy.get('h1').should('have.text', link.text())
     })
-    // Apparently, there's an upper limit to how many times you can do this before Cypress
-    // becomes too stupid to do exactly what it successfully did just moments before. Go figure.
-    // cy.get('@href').then(href => {
-    //   cy.location('pathname').should('eq', href)
-    // })
 
     cy.get('a').contains('Back to the blog').click()
-    cy.get('h1').contains('All Posts')
+    cy.get('h1').should('have.text', 'All Posts')
   })
 })
