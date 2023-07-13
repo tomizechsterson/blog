377c71d34ccc33fc1c34f1a5dbc3bc8a08faeb73
Tomas <tomizechsterson@users.noreply.github.com>
2023-07-12 21:53:56 -0500

    Add tests for all supported sizes in Cypress
    Will pare these down next; many sizes are either identical or close enough

--- cypress/e2e/smoke_test.cy.js
+++ cypress/e2e/smoke_test.cy.js
@@ -1,168 +1,234 @@
 import siteMetadata from '../../data/siteMetadata'
 
+const sizes = [
+  'iphone-se2',
+  'iphone-xr',
+  'iphone-x',
+  'iphone-8',
+  'iphone-7',
+  'iphone-6+',
+  'iphone-6',
+  'ipad-2',
+  'ipad-mini',
+  'macbook-11',
+  'macbook-13',
+  'macbook-15',
+  'macbook-16',
+  'samsung-note9',
+  'samsung-s10',
+  [1280, 720],
+  [1920, 1080]
+]
+
+const setViewPort = (size) => {
+  if (Array.isArray(size)) cy.viewport(size[0], size[1])
+  else cy.viewport(size)
+}
+
 describe('Smoke Test for Site', () => {
-  it('should have header, top nav, and footer on the home page', () => {
-    cy.visit('/')
+  sizes.forEach((size) => {
+    it(`has header, top nav, and footer on the home page (${size})`, () => {
+      setViewPort(size)
 
-    cy.get('title').should('have.text', siteMetadata.title)
-    cy.get('h1').should('have.text', 'Latest Things')
+      cy.visit('/')
 
-    cy.get('header').within(() => {
-      cy.get('a').should('contain.text', 'Blog')
-      cy.get('a').should('contain.text', 'Tags')
-      cy.get('a').should('contain.text', 'Projects')
-      cy.get('a').should('contain.text', 'About')
-    })
+      cy.get('title').should('have.text', siteMetadata.title)
+      cy.get('h1').should('have.text', 'Latest Things')
 
-    cy.get('footer').within(() => {
-      cy.get(`a[href*="mailto:${siteMetadata.email}"]`).should('have.length', 1)
-      cy.get(`a[href="${siteMetadata.github}"]`).should('have.length', 1)
-      cy.get(`a[href="${siteMetadata.linkedin}"]`).should('have.length', 1)
-      cy.get(`a[href="${siteMetadata.pluralsight}"]`).should('have.length', 1)
-      cy.get(`a[href="${siteMetadata.rumble}"]`).should('have.length', 1)
-      cy.get('div').should('contain.text', siteMetadata.author)
-      cy.get('div').should('contain.text', `© ${new Date().getFullYear()}`)
-      cy.get('a').contains(siteMetadata.title).should('have.length', 1)
-    })
-  })
-
-  it('should navigate to first item on home page and back via header', () => {
-    cy.visit('/')
-
-    cy.get('article').should('have.length', 5)
-    cy.get('article')
-      .first()
-      .within(() => {
-        cy.get('a').first().as('link').click()
+      cy.get('header').within(() => {
+        cy.get('a').should('contain.text', 'Blog')
+        cy.get('a').should('contain.text', 'Tags')
+        cy.get('a').should('contain.text', 'Projects')
+        cy.get('a').should('contain.text', 'About')
       })
 
-    cy.get('@link').then((link) => {
-      cy.location('pathname').should('eq', link.attr('href'))
-      cy.get('h1').should('have.text', link.text())
+      cy.get('footer').within(() => {
+        cy.get(`a[href*="mailto:${siteMetadata.email}"]`).should('have.length', 1)
+        cy.get(`a[href="${siteMetadata.github}"]`).should('have.length', 1)
+        cy.get(`a[href="${siteMetadata.linkedin}"]`).should('have.length', 1)
+        cy.get(`a[href="${siteMetadata.pluralsight}"]`).should('have.length', 1)
+        cy.get(`a[href="${siteMetadata.rumble}"]`).should('have.length', 1)
+        cy.get('div').should('contain.text', siteMetadata.author)
+        cy.get('div').should('contain.text', `© ${new Date().getFullYear()}`)
+        cy.get('a').contains(siteMetadata.title).should('have.length', 1)
+      })
     })
 
-    cy.get('[data-cy="header-link"]').click()
-    cy.location('pathname').should('eq', '/')
-  })
+    it(`navigates to first item on home page and back via header (${size})`, () => {
+      setViewPort(size)
 
-  it('should display the about page and some details', () => {
-    cy.visit('/about')
+      cy.visit('/')
 
-    cy.get('h1').should('have.text', 'About')
-    cy.get('[data-cy="author-image"]').should('have.length', 1)
-    cy.get('h3').should('have.text', siteMetadata.author)
-    cy.get('[data-cy="occupation"]').should('have.text', 'Software Engineer')
-    cy.get(`a[href*="mailto:${siteMetadata.email}"]`).should('have.length', 2)
-    cy.get(`a[href="${siteMetadata.github}"]`).should('have.length', 2)
-    cy.get(`a[href="${siteMetadata.linkedin}"]`).should('have.length', 2)
-  })
-
-  it('should display list of blog posts and navigate around them', () => {
-    cy.visit('/blog')
-
-    cy.get('h1').should('have.text', 'All Posts')
-    cy.get('article').should('have.length.gte', 5)
-    cy.get('[data-cy="list-search"]')
-      .type('retro platformer')
-      .should('have.value', 'retro platformer')
-    cy.get('article').should('have.length', 1)
-
-    cy.get('article').within(() => {
-      cy.get('a[href^="/blog"]').as('articleLink').click()
-    })
-
-    cy.get('@articleLink').then((link) => {
-      cy.location('pathname').should('eq', link.attr('href'))
-      cy.get('h1').should('have.text', link.text())
-    })
-    cy.get('div').should('contain.text', 'min read')
-    cy.get('h2').should('contain.text', 'Tags')
-    cy.get('[data-cy="tag-list"]').within(() => {
-      cy.get('a').should('have.length.gt', 0)
-    })
-
-    cy.get('h2').contains('Previous Article')
-    cy.get('[data-cy="previous-article-link"]').as('articleLink').click()
-    cy.get('@articleLink').then((link) => {
-      cy.location('pathname').should('eq', link.attr('href'))
-      cy.get('h1').should('have.text', link.text())
-    })
-
-    cy.get('h2').contains('Next Article')
-    cy.get('[data-cy="next-article-link"]').as('articleLink').click()
-    cy.get('@articleLink').then((link) => {
-      cy.location('pathname').should('eq', link.attr('href'))
-      cy.get('h1').should('have.text', link.text())
-    })
-
-    cy.get('a').contains('Back to the blog').click()
-    cy.get('h1').should('have.text', 'All Posts')
-  })
-
-  it('displays the projects page and navigate around projects', () => {
-    cy.visit('/projects')
-
-    cy.get('h1').should('have.text', 'Projects')
-    cy.get('[data-cy="project-card"]').should('have.length.gte', 3)
-    cy.get('[data-cy="project-card"]')
-      .first()
-      .within(() => {
-        cy.get('h2').within(() => {
-          cy.get('a').as('projectLink').click()
+      cy.get('article').should('have.length', 5)
+      cy.get('article')
+        .first()
+        .within(() => {
+          cy.get('a').first().as('link').click()
         })
+
+      cy.get('@link').then((link) => {
+        cy.location('pathname').should('eq', link.attr('href'))
+        cy.get('h1').should('have.text', link.text())
       })
 
-    cy.get('@projectLink').then((link) => {
-      cy.location('pathname').should('eq', link.attr('href'))
-      cy.get('h1').should('have.text', link.text())
+      cy.get('[data-cy="header-link"]').click()
+      cy.location('pathname').should('eq', '/')
     })
 
-    cy.get('[data-cy="other-projects"]').within(() => {
-      cy.get('li').should('have.length.gte', 2)
-      cy.get('a').first().as('projectLink').click()
+    it(`displays the about page and some details (${size})`, () => {
+      setViewPort(size)
+
+      cy.visit('/about')
+
+      cy.get('h1').should('have.text', 'About')
+      cy.get('[data-cy="author-image"]').should('have.length', 1)
+      cy.get('h3').should('have.text', siteMetadata.author)
+      cy.get('[data-cy="occupation"]').should('have.text', 'Software Engineer')
+      cy.get(`a[href*="mailto:${siteMetadata.email}"]`).should('have.length', 2)
+      cy.get(`a[href="${siteMetadata.github}"]`).should('have.length', 2)
+      cy.get(`a[href="${siteMetadata.linkedin}"]`).should('have.length', 2)
     })
 
-    cy.get('@projectLink').then((link) => {
-      cy.location('pathname').should('eq', link.attr('href'))
-      cy.get('h1').should('have.text', link.text())
-    })
+    it(`displays list of blog posts and navigates around them (${size})`, () => {
+      setViewPort(size)
 
-    cy.get('a').contains('Initial Demo')
-  })
+      cy.visit('/blog')
 
-  it('navigates to the first tag and the first post within', () => {
-    cy.visit('/tags')
+      cy.get('h1').should('have.text', 'All Posts')
+      cy.get('article').should('have.length.gte', 5)
+      cy.get('[data-cy="list-search"]')
+        .type('retro platformer')
+        .should('have.value', 'retro platformer')
+      cy.get('article').should('have.length', 1)
 
-    cy.get('h1').should('have.text', 'Tags')
-    cy.get('a[href^="/tags/"]').first().as('tagLink').click()
-
-    cy.get('@tagLink').then((link) => {
-      cy.location('pathname').should('eq', link.attr('href'))
-      cy.get('h1').contains(link.text(), { matchCase: false })
-    })
-
-    cy.get('article')
-      .first()
-      .within(() => {
+      cy.get('article').within(() => {
         cy.get('a[href^="/blog"]').as('articleLink').click()
       })
 
-    cy.get('@articleLink').then((link) => {
-      cy.location('pathname').should('eq', link.attr('href'))
-      cy.get('h1').should('have.text', link.text())
+      cy.get('@articleLink').then((link) => {
+        cy.location('pathname').should('eq', link.attr('href'))
+        cy.get('h1').should('have.text', link.text())
+      })
+      cy.get('div').should('contain.text', 'min read')
+      cy.get('h2').should('contain.text', 'Tags')
+      cy.get('[data-cy="tag-list"]').within(() => {
+        cy.get('a').should('have.length.gt', 0)
+      })
+
+      cy.get('h2').contains('Previous Article')
+      cy.get('[data-cy="previous-article-link"]').as('articleLink').click()
+      cy.get('@articleLink').then((link) => {
+        cy.location('pathname').should('eq', link.attr('href'))
+        cy.get('h1').should('have.text', link.text())
+      })
+
+      cy.get('h2').contains('Next Article')
+      cy.get('[data-cy="next-article-link"]').as('articleLink').click()
+      cy.get('@articleLink').then((link) => {
+        cy.location('pathname').should('eq', link.attr('href'))
+        cy.get('h1').should('have.text', link.text())
+      })
+
+      cy.get('a').contains('Back to the blog').click()
+      cy.get('h1').should('have.text', 'All Posts')
     })
-  })
 
-  it('should click on each header nav item', () => {
-    cy.visit('/')
+    it(`displays the projects page and navigates around projects (${size})`, () => {
+      setViewPort(size)
 
-    cy.get('a').contains('Blog').click()
-    cy.location('pathname').should('eq', '/blog')
-    cy.get('a').contains('Tags').click()
-    cy.location('pathname').should('eq', '/tags')
-    cy.get('a').contains('Projects').click()
-    cy.location('pathname').should('eq', '/projects')
-    cy.get('a').contains('About').click()
-    cy.location('pathname').should('eq', '/about')
+      cy.visit('/projects')
+
+      cy.get('h1').should('have.text', 'Projects')
+      cy.get('[data-cy="project-card"]').should('have.length.gte', 3)
+      cy.get('[data-cy="project-card"]')
+        .first()
+        .within(() => {
+          cy.get('h2').within(() => {
+            cy.get('a').as('projectLink').click()
+          })
+        })
+
+      cy.get('@projectLink').then((link) => {
+        cy.location('pathname').should('eq', link.attr('href'))
+        cy.get('h1').should('have.text', link.text())
+      })
+
+      cy.get('[data-cy="other-projects"]').within(() => {
+        cy.get('li').should('have.length.gte', 2)
+        cy.get('a').first().as('projectLink').click()
+      })
+
+      cy.get('@projectLink').then((link) => {
+        cy.location('pathname').should('eq', link.attr('href'))
+        cy.get('h1').should('have.text', link.text())
+      })
+
+      cy.get('a').contains('Initial Demo')
+    })
+
+    it(`navigates to the first tag and the first post within (${size})`, () => {
+      setViewPort(size)
+
+      cy.visit('/tags')
+
+      cy.get('h1').should('have.text', 'Tags')
+      cy.get('a[href^="/tags/"]').first().as('tagLink').click()
+
+      cy.get('@tagLink').then((link) => {
+        cy.location('pathname').should('eq', link.attr('href'))
+        cy.get('h1').contains(link.text(), { matchCase: false })
+      })
+
+      cy.get('article')
+        .first()
+        .within(() => {
+          cy.get('a[href^="/blog"]').as('articleLink').click()
+        })
+
+      cy.get('@articleLink').then((link) => {
+        cy.location('pathname').should('eq', link.attr('href'))
+        cy.get('h1').should('have.text', link.text())
+      })
+    })
+
+    it('clicks on each header nav item', () => {
+      setViewPort(size)
+
+      cy.window().then((win) => {
+        cy.visit('/')
+
+        if (win.innerWidth < 640) {
+          cy.get('[data-cy="hamburger-menu"]').click()
+          cy.get('[data-cy="mobile-nav"]').within(() => {
+            cy.get('a').contains('Blog').click()
+          })
+          cy.location('pathname').should('eq', '/blog')
+          cy.get('[data-cy="hamburger-menu"]').click()
+          cy.get('[data-cy="mobile-nav"]').within(() => {
+            cy.get('a').contains('Tags').click()
+          })
+          cy.location('pathname').should('eq', '/tags')
+          cy.get('[data-cy="hamburger-menu"]').click()
+          cy.get('[data-cy="mobile-nav"]').within(() => {
+            cy.get('a').contains('Projects').click()
+          })
+          cy.location('pathname').should('eq', '/projects')
+          cy.get('[data-cy="hamburger-menu"]').click()
+          cy.get('[data-cy="mobile-nav"]').within(() => {
+            cy.get('a').contains('About').click()
+          })
+          cy.location('pathname').should('eq', '/about')
+        } else {
+          cy.get('a').contains('Blog').click()
+          cy.location('pathname').should('eq', '/blog')
+          cy.get('a').contains('Tags').click()
+          cy.location('pathname').should('eq', '/tags')
+          cy.get('a').contains('Projects').click()
+          cy.location('pathname').should('eq', '/projects')
+          cy.get('a').contains('About').click()
+          cy.location('pathname').should('eq', '/about')
+        }
+      })
+    })
   })
 })
